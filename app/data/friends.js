f81b7d575dfc4754d03cd4cbd262bc96d75a9c62
YvelStLaurent <rachaelbelle@gmail.com>
2019-05-13 19:45:51 -0400

    html built, working on apis and js

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendArray = [
+  {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+   ]
+},{
+  "name": "Rachael Hutchens",
+  "photo": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/525593_10151100042667388_1218857598_n.jpg?_nc_cat=102&_nc_ht=scontent-lga3-1.xx&oh=196fe3f5636e1875dffe3707be27a0d1&oe=5D6BFCAB",
+  "scores": [
+  "3",
+  "2",
+  "2",
+  "2",
+  "4",
+  "1",
+  "4",
+  "1",
+  "3",
+  "1"
+  ]
+  },
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
