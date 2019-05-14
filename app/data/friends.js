6ef4da1fad2f4d9db334469fec4dd7d038a2aec8
YvelStLaurent <rachaelbelle@gmail.com>
2019-05-13 20:09:44 -0400

    cleaning up code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,3 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
 
 var friendArray = [
   {
