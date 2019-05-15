c26e8dff6f7278bbcd245516b222b5e85a2c7391
YvelStLaurent <rachaelbelle@gmail.com>
2019-05-15 13:34:50 -0400

    apiRoute update

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,18 +3,33 @@ var friendData = require("../data/friends");
 
 module.exports = function(app) {
 
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
   });
 
-  // app.post("/api/friends", function(req, res) {
-  //   friendData.push(req.body);
-  //   res.json(true);
-  // });
-  // };
 
-  // app.post("/api/clear", function(req, res) {
-  //   friendData.length = 0;
-  //   res.json({ ok: true });
-  // });
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+
+
+  app.post("/api/friends", function(req, res) {
+    friendData.push(req.body);
+    res.json(true);
+  });
+
+
+  app.post("/api/clear", function(req, res) {
+    friendData.length = 0;
+    res.json({ ok: true });
+  });
 };
