f8f5886459c8d2a8a48de8de48b27b7684980baf
YvelStLaurent <rachaelbelle@gmail.com>
2019-05-15 18:08:21 -0400

    added 2 additional names to friends.js, more things work, I feel better about this

--- data/friends.js
+++ data/friends.js
@@ -30,7 +30,37 @@ var friendArray = [
   "5",
   "5"
   ]
-  }
+  },{
+    "name": "Yvel Saint Laurent",
+    "photo": "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/53541008_10158183885362388_7471704397236404224_o.jpg?_nc_cat=106&_nc_ht=scontent-lga3-1.xx&oh=9c8d1d385d6aec7164c12f7fc9a1f7f4&oe=5D561C81",
+    "scores": [
+    "4",
+    "3",
+    "3",
+    "2",
+    "5",
+    "3",
+    "2",
+    "2",
+    "4",
+    "5"
+    ]
+    },{
+    "name": "John Kozyrakis",
+    "photo": "https://i.pinimg.com/736x/7d/b3/56/7db3563c1ee9eadfe57b36aa4c381c5a--instrumental-music-artists.jpg",
+    "scores": [
+    "5",
+    "1",
+    "3",
+    "4",
+    "1",
+    "1",
+    "2",
+    "1",
+    "1",
+    "5"
+    ]
+    },
 ];
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendArray;
